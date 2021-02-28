d97069d8d9f06eb29c5bb79cdad0f4986655a888
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-02-28 20:18:07 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
     return ( 
 	<div className="App">
 		<div className="container">
-<h1>Weather App</h1>
+
+	<Weather />
     	<footer>
   This project was coded by Anna Vardaxoglou and is{" "}   
       	<a href="https://github.com/AnnaVarda/weather-app-final" target="_blank">
