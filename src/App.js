bccd5995636f80b9cfd0c0f0f20176ddc919c4ad
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-02-28 19:14:12 +0200

    Added info url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 
 export default function App() {
     return ( 
-    <div className="App">
+<div className="App">
 Weather App
-    </div>)
+    <footer>
+  This project was coded by Anna Vardaxoglou and is{" "}   
+        <a href="https://github.com/AnnaVarda/weather-app-final" target="_blank">
+        open-sourced on GitHub
+        </a>  
+    </footer> 
+</div>)
 }
