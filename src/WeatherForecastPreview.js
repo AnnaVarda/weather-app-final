32285b1cdbf698bb466c6b6b3d9b6fefa612bf2e
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-03-01 11:05:26 +0200

    Forecast

---
+++ src/WeatherForecastPreview.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function WeatherForecastPreview(props) {
+	function hours() {
+		let date = new Date(props.data.dt * 1000);
+		let hours = date.getHours();
+		return `${hours}:00`;
+	}
+
+	function temperature() {
+		let temperature = Math.round(props.data.main.temp)
+		
+		return `${temperature}°C`;
+	}
+
+  return (
+    <div className="WeatherForecastPreview col">   
+			{hours()}
+
+			<img 
+    src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+    />
+
+			{temperature()}
+		</div>
+ );
+}
+
+
+
+
